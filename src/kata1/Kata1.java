200e0d1f34bdc035ffa071c0eb969f9d463fa0a0
Simon <Simon@DESKTOP-JG8QGEC>
2021-09-26 18:50:24 +0100

    v3

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,15 +1,9 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1997, 8, 6);
-        Person p = new Person("Oliver", date);
+        Person p = new Person("Oliver", "1997-08-06");
         System.out.println(p.getName()+ " tiene "+ p.getAge());
     }
     
